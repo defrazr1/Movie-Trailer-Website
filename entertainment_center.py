f4f9e34ab05331102e726f1d52b689ba4caccc24
DemFraz <dfdefrazr@gmail.com>
2017-01-07 19:09:14 -0800

    adding movie objects

---
+++ entertainment_center.py
@@ -0,0 +1,3 @@
+blazing_saddles = media.Movie("Blazing Saddles",
+                              "https://upload.wikimedia.org/wikipedia/en/7/7b/Blazing_saddles_movie_poster.jpg",
+                              "https://youtu.be/VKayG1TrfuE")
